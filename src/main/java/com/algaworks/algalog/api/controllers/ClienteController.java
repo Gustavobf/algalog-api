21a86cf995931e8b81931ad35e7fcf2e943fd777
Gustavo Batista <gustavobatistafig@hotmail.com>
2021-05-26 19:27:38 -0300

    Utilizando JPA

--- src/main/java/com/algaworks/algalog/api/controllers/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controllers;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +14,14 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteController {
-	
+
+	@PersistenceContext
+	private EntityManager manager;
+
 	@GetMapping
 	public List<Cliente> listar() {
-		
-		var cliente1 = new Cliente(1L, "Joao", "joaodascouves@algaworks.com", "34 99999-1111");
-		var cliente2 = new Cliente(2L, "Maria", "mariadasilva@algaworks.com", "34 99999-2222");
-		
-		return Arrays.asList(cliente1, cliente2);
+
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
-	
+
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,19 +1,35 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Cliente {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String nome;
+
 	private String email;
+
+	@Column(name = "fone")
 	private String telefone;
 
 }
