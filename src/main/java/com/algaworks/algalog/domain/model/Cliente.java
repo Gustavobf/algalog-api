b375bc4bd68549bba7e8efdcbee005fd94a4add2
Gustavo Batista <gustavobatistafig@hotmail.com>
2021-06-28 21:23:02 -0300

    Usando ModelMapper para transformar objetos

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -26,7 +26,6 @@ import lombok.Setter;
 @Entity
 public class Cliente {
 
-	@NotNull(groups = ValidationGroups.ClienteId.class)
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
